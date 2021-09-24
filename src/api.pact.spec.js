cab3625a75e9e095a52f0e20a6e08799a6944116
james-shirtcliffe-gw <James.Shirtcliffe@gwplc.com>
2021-09-24 16:52:59 +0100

    add a colour field

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -16,7 +16,7 @@ describe('API Pact test', () => {
   describe('retrieving a product', () => {
     test('ID 10 exists', async () => {
       // Arrange
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees'}
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees' }
 
       // Uncomment to see this fail
       // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
@@ -34,7 +34,7 @@ describe('API Pact test', () => {
         willRespondWith: {
           status: 200,
           headers: {
-            'Content-Type': regex({generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*'}),
+            'Content-Type': regex({ generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*' }),
           },
           body: like(expectedProduct),
         },
@@ -50,32 +50,32 @@ describe('API Pact test', () => {
 
     test('product does not exist', async () => {
 
-        // set up Pact interactions
-        await mockProvider.addInteraction({
-          state: 'a product with ID 11 does not exist',
-          uponReceiving: 'a request to get a product',
-          withRequest: {
-            method: 'GET',
-            path: '/product/11',
-            headers: {
-              'Authorization': like('Bearer 2019-01-14T11:34:18.045Z')
-            }
-          },
-          willRespondWith: {
-            status: 404
-          },
-        });
+      // set up Pact interactions
+      await mockProvider.addInteraction({
+        state: 'a product with ID 11 does not exist',
+        uponReceiving: 'a request to get a product',
+        withRequest: {
+          method: 'GET',
+          path: '/product/11',
+          headers: {
+            'Authorization': like('Bearer 2019-01-14T11:34:18.045Z')
+          }
+        },
+        willRespondWith: {
+          status: 404
+        },
+      });
 
-        const api = new API(mockProvider.mockService.baseUrl);
+      const api = new API(mockProvider.mockService.baseUrl);
 
-        // make request to Pact mock server
-        await expect(api.getProduct('11')).rejects.toThrow('Request failed with status code 404');
+      // make request to Pact mock server
+      await expect(api.getProduct('11')).rejects.toThrow('Request failed with status code 404');
     });
   });
   describe('retrieving products', () => {
     test('products exists', async () => {
       // set up Pact interactions
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees' }
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', colour: 'red' }
 
       await mockProvider.addInteraction({
         state: 'products exist',
@@ -90,7 +90,7 @@ describe('API Pact test', () => {
         willRespondWith: {
           status: 200,
           headers: {
-            'Content-Type': regex({generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*'}),
+            'Content-Type': regex({ generate: 'application/json; charset=utf-8', matcher: 'application/json;?.*' }),
           },
           body: eachLike(expectedProduct),
         },
